847cf5622cf7e1d684f82e30eb5bacda91afba7a
Ricky <romalax248130@gmail.com>
2024-04-10 19:59:16 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var noBtn = document.getElementById("noBtn");
+
+    noBtn.addEventListener("click", function() {
+        var confirmation = confirm("Please don't say no 🥺");
+    });
+
+});
